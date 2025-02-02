6bd5c494d2d2738b83ac7472c603a1025ca12320
Indiho-rbg <sonoky1995@gmail.com>
2025-02-02 11:48:35 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,27 +1,15 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
-# Вставте ваш токен сюди
-TOKEN = '7592348192:AAGE24v6WWSKRSIclap7iUATad5kqdimYSU'
+from telegram.ext import CommandHandler, CallbackContext, Updater
 
-# Функція для команди /start
 def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Привіт! Я готовий до роботи!')
+    update.message.reply_text("Привіт! Я працюю!")
 
-# Основна функція, яка запускає бота
-def main():
-    updater = Updater(TOKEN, use_context=True)
-    
-    # Отримуємо диспетчер для обробки команд
-    dp = updater.dispatcher
-    
-    # Додаємо команду /start
-    dp.add_handler(CommandHandler("start", start))
-    
-    # Запускаємо бота
+def handler(request):
+    token = 'YOUR_BOT_TOKEN'
+    updater = Updater(token, use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+
     updater.start_polling()
 
-    # Додаємо обробку сигнала для зупинки бота
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+    return "OK"
