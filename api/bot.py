151026335e7ae17326a70badb820cc86cb5073d4
Indiho-rbg <sonoky1995@gmail.com>
2025-02-02 11:50:05 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -5,7 +5,7 @@ def start(update: Update, context: CallbackContext):
     update.message.reply_text("Привіт! Я працюю!")
 
 def handler(request):
-    token = 'YOUR_BOT_TOKEN'
+    token = '7592348192:AAGE24v6WWSKRSIclap7iUATad5kqdimYSU'
     updater = Updater(token, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
