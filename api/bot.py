6d95c4f7616058dbd2c4dfb2fa470491542cc3e9
Indiho-rbg <sonoky1995@gmail.com>
2025-02-02 11:07:29 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,22 +1,19 @@
-# bot.py
-
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Функція для обробки команд
+# Ваш токен Telegram бота
+TOKEN = '7592348192:AAFyqIJnZTvjjzShu_az9emIKZKkkZFcQFk'
+
+# Функція, яка обробляє команду /start
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привіт!')
+    update.message.reply_text('Привіт! Я твій бот.')
 
-# Створення обробника команд
-def main():
-    updater = Updater("7592348192:AAFyqIJnZTvjjzShu_az9emIKZKkkZFcQFk")
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-
-    updater.start_polling()
-    updater.idle()
-
-# Головна точка входу
+# Головна функція для обробки запитів
 def handler(request):
-    main()
-    return "Bot is running!"
+    if request.method == "POST":
+        # Тут ви можете додати код для обробки запиту, наприклад запуск бота
+        updater = Updater(TOKEN, use_context=True)
+        updater.dispatcher.add_handler(CommandHandler("start", start))
+        updater.start_polling()
+        return "OK"
+    return "Error"
