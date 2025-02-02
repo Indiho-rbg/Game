495ce37280ce60eca939baed11c9e628175ce2d9
Indiho-rbg <sonoky1995@gmail.com>
2025-02-02 13:57:53 +0200

    Update bot.py

--- api/bot.py
+++ api/bot.py
@@ -1,37 +1,38 @@
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, CallbackContext, Dispatcher
 from flask import Flask, request
 
-app = Flask(__name__)
-
-@app.route('/api/bot', methods=['POST', 'GET'])
-def handle_request():
-    return "Bot is running!", 200
-
-# Токен вашого бота
 TOKEN = '7592348192:AAGE24v6WWSKRSIclap7iUATad5kqdimYSU'
 
-# Функція для обробки команд
+# Налаштовуємо Flask
+app = Flask(__name__)
+
+# Ініціалізуємо Telegram бота
+updater = Updater(TOKEN, use_context=True)
+dispatcher: Dispatcher = updater.dispatcher
+
+# Функція для обробки команди /start
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Привіт! Я бот!")
 
-# Обробка запиту від Telegram
+dispatcher.add_handler(CommandHandler("start", start))
+
+# Вебхук для отримання оновлень від Telegram
 @app.route(f'/{TOKEN}', methods=['POST'])
 def webhook():
     json_str = request.get_data().decode("UTF-8")
     update = Update.de_json(json_str, updater.bot)
     dispatcher.process_update(update)
-    return 'OK'
+    return 'OK', 200
 
-# Запуск бота
-if __name__ == '__main__':
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-    dispatcher.add_handler(CommandHandler("start", start))
-    
-    # Встановлюємо вебхук
-    updater.bot.setWebhook(f'https://game-three-puce.vercel.app/{TOKEN}')
-    app.run()
+# Встановлюємо вебхук при завантаженні
+updater.bot.setWebhook(f'https://game-three-puce.vercel.app/{TOKEN}')
 
-    handler = app  # ВАЖЛИВО для Vercel
+# Головна сторінка для перевірки роботи
+@app.route('/')
+def home():
+    return "Bot is running!", 200
+
+# ВАЖЛИВО! Це потрібно для Vercel
+handler = app
